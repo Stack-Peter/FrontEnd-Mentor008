f9f08c85caf07e7436a9f17c1857d400003397ab
Pedro Marcos F <stack.peter21@gmail.com>
2021-08-25 22:01:25 -0300

    second commit

--- README.md
+++ README.md
@@ -6,17 +6,11 @@ This is a solution to the [Tip calculator app challenge on Frontend Mentor](http
 
 - [Overview](#overview)
   - [The challenge](#the-challenge)
-  - [Screenshot](#screenshot)
   - [Links](#links)
 - [My process](#my-process)
   - [Built with](#built-with)
   - [What I learned](#what-i-learned)
-  - [Continued development](#continued-development)
-  - [Useful resources](#useful-resources)
 - [Author](#author)
-- [Acknowledgments](#acknowledgments)
-
-**Note: Delete this note and update the table of contents based on what sections you keep.**
 
 ## Overview
 
@@ -30,8 +24,8 @@ Users should be able to:
 
 ### Links
 
-- Solution URL: [Add solution URL here](https://your-solution-url.com)
-- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+- Solution URL: [GitHub](https://github.com/Stack-Peter/FrontEnd-Mentor008)
+- Live Site URL: [Here](https://vigilant-keller-ee2713.netlify.app)
 
 ## My process
 
--- main.js
+++ main.js
@@ -6,15 +6,21 @@ const htmlTip = document.querySelector('.num');
 const htmlTotal = document.querySelector('.num-two');
 const custom = document.querySelector('.custom');
 const numPeople = document.querySelector('[name="numberPeople"]')
-
+numPeople.value = +1
 function handleChange(e) {
     const value = e.target.value
-
-
     custom.addEventListener('change', (e) => {
+        radio.forEach((i) => {
+            if (i.classList.contains('selected')) {
+                i.classList.remove('selected')
+                i.classList.add('unselected')
+            }
+        })
         const customVal = +e.target.value
         const anotherVal = (+value * (customVal / 100))
         const newValue = anotherVal + +value
+        htmlTip.innerHTML = `$${(customVal).toFixed(2)}`
+        htmlTotal.innerHTML = `$${(newValue).toFixed(2)}`
         const numPeople = document.querySelector('[name="numberPeople"]')
         numPeople.addEventListener('keyup', (event) => {
             const num = +event.target.value
@@ -26,9 +32,16 @@ function handleChange(e) {
 
     radio.forEach((item, index) => {
         item.nextElementSibling.addEventListener('click', () => {
+            radio.forEach((i) => {
+                i.classList.remove('selected')
+            })
+            if (!item.classList.contains('selected')) {
+                item.classList.add('selected')
+            }
             const valueRadio = (+value * (+item.id / 100))
             const newValue = valueRadio + +value
-            console.log(valueRadio)
+            htmlTip.innerHTML = `$${(valueRadio / numPeople.value).toFixed(2)}`
+            htmlTotal.innerHTML = `$${(newValue / numPeople.value).toFixed(2)}`
             numPeople.addEventListener('keyup', (event) => {
                 const num = +event.target.value
                 const tipPerson = valueRadio / num
